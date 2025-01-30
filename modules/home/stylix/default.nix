09e17a142e8858eee0a680b23cd6fce3b8f4961c
JRhin <github@jrhin.com>
2025-01-31 00:23:22 +0100

    flake update

--- modules/home/stylix/default.nix
+++ modules/home/stylix/default.nix
@@ -9,7 +9,7 @@
 
   stylix.fonts = {
     monospace = {
-      package = pkgs.nerdfonts.override {fonts = ["FiraCode"];};
+      package = pkgs.nerd-fonts.fira-code;
       name = "FiraCode Nerd Font Mono";
     };
     
--- modules/system/pipewire/default.nix
+++ modules/system/pipewire/default.nix
@@ -1,13 +1,15 @@
 {...}:
 
 {
-  hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    jack.enable = true;
+  services = {
+    pulseaudio.enable = false;
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      jack.enable = true;
+    };
   };
 }
