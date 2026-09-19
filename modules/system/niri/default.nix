bf089f867b0c8a86f15d7aba23b3f0b12a88b44a
JRhin <github@jrhin.com>
2026-09-19 12:14:16 +0200

    fixed xwayland

--- modules/system/niri/default.nix
+++ modules/system/niri/default.nix
@@ -1,5 +1,11 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   programs.niri.enable = true;
+
+  # X11 apps under niri: niri starts it on demand when it is in PATH
+  environment.systemPackages = [ pkgs.xwayland-satellite ];
+
+  # Run Electron/Chromium apps (Discord, Obsidian, ...) natively on Wayland
+  environment.sessionVariables.NIXOS_OZONE_WL = "1";
 }
